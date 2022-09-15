ec8c574f853b3e09cf84ee8ac0261049a80ba8ea
505laura <106522200+505laura@users.noreply.github.com>
2022-09-15 16:06:16 +1000

    Add function to create 9 time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
 const today = moment();
 $('#currentDay').text (today.format('dddd, MMMM Do YYYY'));
+
+for (let hour = 9; hour < 18; hour++) {
+    $('.container').append(`<div class="row">
+    <div class="hour col-2 text-center p-3">9am</div>
+    <div class="description col-8 past p-0"><textarea class="col-12 h-100"></textarea></div>
+    <button class="saveBtn col-2"><i class="fas fa-save"></i></button>
+    </div>`);
+}
+
