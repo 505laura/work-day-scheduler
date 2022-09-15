4f7747af41d27814dafcb52867be3b3ff62ac44b
505laura <106522200+505laura@users.noreply.github.com>
2022-09-15 16:58:11 +1000

    Save text description to local storage as a string

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+let events = [];
+
 const today = moment();
 $('#currentDay').text (today.format('dddd, MMMM Do YYYY'));
 
@@ -5,7 +7,7 @@ for (let hour = 9; hour < 18; hour++) {
     $('.container').append(`<div class="row">
     <div class="hour col-2 text-center p-3">${workDayHour(hour)}</div>
     <div class="description col-8 ${pastPresentFuture(hour)} p-0"><textarea class="col-12 h-100"></textarea></div>
-    <button class="saveBtn col-2"><i class="fas fa-save"></i></button>
+    <button data-hour="${hour}" class="saveBtn col-2"><i class="fas fa-save"></i></button>
     </div>`);
 }
 
@@ -25,3 +27,12 @@ function pastPresentFuture(time) {
         return 'future';
     }
 }
+
+function saveText(event) {
+    const buttonClicked = $(event.target);
+    const el = buttonClicked.siblings('.description').children().eq(0);
+    events[buttonClicked.data('hour')] = el.val();
+    localStorage.setItem(moment().format('dddd, MMMM Do'), JSON.stringify(events));
+}
+
+$('button').on('click', saveText);
