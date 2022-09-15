cb1ea0076ccb51b2b1d43dd867383aca13335d00
505laura <106522200+505laura@users.noreply.github.com>
2022-09-15 16:28:57 +1000

    Add function to change time block colours based on time of day

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,25 @@ $('#currentDay').text (today.format('dddd, MMMM Do YYYY'));
 
 for (let hour = 9; hour < 18; hour++) {
     $('.container').append(`<div class="row">
-    <div class="hour col-2 text-center p-3">9am</div>
-    <div class="description col-8 past p-0"><textarea class="col-12 h-100"></textarea></div>
+    <div class="hour col-2 text-center p-3">${workDayHour(hour)}</div>
+    <div class="description col-8 ${pastPresentFuture(hour)} p-0"><textarea class="col-12 h-100"></textarea></div>
     <button class="saveBtn col-2"><i class="fas fa-save"></i></button>
     </div>`);
 }
 
+function workDayHour(time) {
+    const now = moment();
+    now.hours(time);
+    return now.format('ha');
+}
+
+function pastPresentFuture(time) {
+    const currentTime = moment().hour();
+    if (currentTime == time) {
+        return 'present';
+    } else if (currentTime > time) {
+        return 'past';
+    } else {
+        return 'future';
+    }
+}
